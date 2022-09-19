7cc598fdb8a69c97b0625a001c69ad2a23d10f04
Dr. Wichy Ph <88720979+semisailom@users.noreply.github.com>
2022-09-19 14:15:39 +0700

    Update api.js

--- api.js
+++ api.js
@@ -22,11 +22,11 @@ const productsContainer = document.querySelector('.products-grid');
                     <img class="product-img" src="${product.c[4].v}" alt="${product[0]}">
                 </div>
                 <div class="product-card__info">
-                    <h2 class="product-brand">Marca: ${product.c[0].v}</h2>
-                    <p class="product-text">Modelo: ${product.c[1].v}</p>
-                    <p class="product-text">Año: ${product.c[3].v}</p>
+                    <h2 class="product-brand">Brand: ${product.c[0].v}</h2>
+                    <p class="product-text">Model: ${product.c[1].v}</p>
+                    <p class="product-text">Year: ${product.c[3].v}</p>
                     <a class="product-link" href="${product.c[5].v}">
-                    <button class="product-btn">Ver más</button>
+                    <button class="product-btn">More</button>
                     </a>
                 </div>
             `;
